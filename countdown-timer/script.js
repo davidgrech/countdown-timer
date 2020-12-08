5044e5fa9ccc026d3e835b2344c0a5983f2a6c08
David <david.grech26@gmail.com>
2020-12-08 17:24:20 +0000

    Update script.js

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,14 +1,27 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
 const newYears = '1 Jan 2021';
 
 function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const seconds = (newYearsDate - currentDate) * 1000;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    const days = Math.floor(seconds / 3600 / 24);
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
-    console.log(newYearsDate - currentDate);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+
 }
 
 //First call
